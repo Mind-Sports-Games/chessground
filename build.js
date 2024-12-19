95f9c08090016274ee26bff9a7d82bbf583ffeee
Vincent FROCHOT <vincentfrochot@gmail.com>
2024-12-19 11:31:42 +0100

    fix: remove traces of merge

--- build.js
+++ build.js
@@ -1,25 +1,4 @@
 import * as cps from 'node:child_process';
-<<<<<<< HEAD
-import * as ps from 'node:process';
-
-const consumerRoot = process.env.CONSUMER_ROOT;
-const args = ps.argv.slice(2);
-
-if (args.includes('--link')) {
-  cps.execSync('pnpm link --dir ' + consumerRoot);
-  console.log('\x1b[36m%s\x1b[0m', 'Linked to ' + consumerRoot);
-}
-if (args.includes('--bundle')) {
-  cps.execSync(
-    'esbuild src/chessground.ts --bundle --format=esm --outfile=' +
-      consumerRoot +
-      '/node_modules/chessground/dist/chessground.min.js',
-  );
-  console.log(
-    '\x1b[32m%s\x1b[0m',
-    'Compiled and built ' + consumerRoot + '/node_modules/chessground/dist/chessground.min.js',
-  );
-=======
 
 const red = '\x1b[31m%s\x1b[0m';
 const green = '\x1b[32m%s\x1b[0m';
@@ -48,5 +27,4 @@ if (args.includes('--bundle')) {
     { stdio: 'inherit' },
   );
   console.log(green, `compiled and built ${linkedProject}/node_modules/chessground/dist/chessground.min.js`);
->>>>>>> origin/master
 }
