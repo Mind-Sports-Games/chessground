ce0750c6368c541e71f3f6658c130e164489e564
JamesHeppell <jheppell100@hotmail.com>
2025-01-23 10:03:46 +0000

    support crawford rule and make doubling cube unavailable

--- src/wrap.ts
+++ src/wrap.ts
@@ -218,25 +218,29 @@ export function renderWrap(element: HTMLElement, s: HeadlessState, relative: boo
   }
 
   if (s.variant === 'backgammon' || s.variant === 'hyper' || s.variant === 'nackgammon') {
-    if (s.doublingCube) {
-      container.appendChild(renderDoublingCube(s.doublingCube));
-      if (s.cubeActions) {
-        if (s.cubeActions.includes('offer'))
-          container.appendChild(
-            renderCubeAction('double', s.turnPlayerIndex + ' left', s.turnPlayerIndex === s.myPlayerIndex),
-          );
-        if (s.cubeActions.includes('offer'))
-          container.appendChild(
-            renderCubeAction('roll', s.turnPlayerIndex + ' right', s.turnPlayerIndex === s.myPlayerIndex),
-          );
-        if (s.cubeActions.includes('reject'))
-          container.appendChild(
-            renderCubeAction('drop', s.turnPlayerIndex + ' left', s.turnPlayerIndex === s.myPlayerIndex),
-          );
-        if (s.cubeActions.includes('accept'))
-          container.appendChild(
-            renderCubeAction('take', s.turnPlayerIndex + ' right', s.turnPlayerIndex === s.myPlayerIndex),
-          );
+    if (s.multiPointState) {
+      if (s.doublingCube === undefined) {
+        container.appendChild(renderDoublingCube({ owner: 'both', value: 0 }, true));
+      } else {
+        container.appendChild(renderDoublingCube(s.doublingCube, false));
+        if (s.cubeActions) {
+          if (s.cubeActions.includes('offer'))
+            container.appendChild(
+              renderCubeAction('double', s.turnPlayerIndex + ' left', s.turnPlayerIndex === s.myPlayerIndex),
+            );
+          if (s.cubeActions.includes('offer'))
+            container.appendChild(
+              renderCubeAction('roll', s.turnPlayerIndex + ' right', s.turnPlayerIndex === s.myPlayerIndex),
+            );
+          if (s.cubeActions.includes('reject'))
+            container.appendChild(
+              renderCubeAction('drop', s.turnPlayerIndex + ' left', s.turnPlayerIndex === s.myPlayerIndex),
+            );
+          if (s.cubeActions.includes('accept'))
+            container.appendChild(
+              renderCubeAction('take', s.turnPlayerIndex + ' right', s.turnPlayerIndex === s.myPlayerIndex),
+            );
+        }
       }
     }
     if (s.multiPointState) {
@@ -317,10 +321,10 @@ function renderMultiPointTarget(target: number): HTMLElement {
   return el;
 }
 
-function renderDoublingCube(dCube: DoublingCube): HTMLElement {
+function renderDoublingCube(dCube: DoublingCube, unavailable: boolean): HTMLElement {
   const el = createEl('cg-doubling-cube', dCube.owner);
   const doubeCubeClass = ['zero', 'one', 'two', 'three', 'four', 'five', 'six'];
-  const cube = createEl('cube', doubeCubeClass[dCube.value]);
+  const cube = createEl('cube', doubeCubeClass[dCube.value] + (unavailable ? ' unavailable' : ''));
   el.appendChild(cube);
   return el;
 }
