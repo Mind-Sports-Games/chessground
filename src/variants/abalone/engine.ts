669997229361485972be05f5415ca25daf26c91f
Alex <alex.borello@direct-fleet.com>
2025-03-26 22:29:58 +0100

    [Abalone] engine.ts::computeMoveImpact, computeMoveVectorPostMove

--- src/variants/abalone/engine.ts
+++ src/variants/abalone/engine.ts
@@ -1,13 +1,13 @@
 import type * as cg from '../../types';
+import {Pos} from "../../types";
 
 import {candidateLineDirs, deducePotentialSideDirs, DiagonalDirectionString, getDirectionString, inverseDirection, isMoveInLine, move,} from './directions';
 import type {MoveImpact, MoveVector} from './types';
-import {add, div, getRotatedKeepNorm, isCell, key2pos, mult, getNeighVectors, norm, pos2key, sub} from "./util";
-import {Pos} from "../../types";
+import {add, div, getNeighVectors, getRotatedKeepNorm, isCell, key2pos, mult, norm, pos2key, sub} from "./util";
 
 // Computes the effect of a move on the board before it is made
 export const computeMoveImpact = (d: cg.BoardDimensions, pieces: cg.Pieces, orig: cg.Key, dest: cg.Key): MoveImpact | undefined => {
-	if (pieces.has(orig)) {
+	if (pieces.has(orig) && !pieces.has(dest)) {
 		const from = key2pos(orig);
 		const to = key2pos(dest);
 		const vect = sub(to, from);
@@ -81,7 +81,6 @@ export const computeMoveImpact = (d: cg.BoardDimensions, pieces: cg.Pieces, orig
 					
 					var a = from;
 					var ka = orig;
-					var pa = pieces.get(ka);
 					var k = 0;
 					
 					while (k < n) {
@@ -93,12 +92,11 @@ export const computeMoveImpact = (d: cg.BoardDimensions, pieces: cg.Pieces, orig
 						if (pieces.has(kb)) return undefined;
 						
 						diff.set(ka, undefined);
-						diff.set(kb, pa);
+						diff.set(kb, pieces.get(ka));
 						dests.push(kb);
 						
 						a = add(from, mult(++k, uvect));
 						ka = pos2key(a);
-						pa = pieces.get(ka);
 					}
 					
 					return {
@@ -118,94 +116,83 @@ export const computeMoveImpact = (d: cg.BoardDimensions, pieces: cg.Pieces, orig
 };
 
 // Computes a move vector after the move has been made
-export const computeMoveVectorPostMove = (d: cg.BoardDimensions, pieces: cg.Pieces, orig: cg.Key, dest: cg.Key): MoveVector | undefined => {
-	const from = key2pos(orig);
-	const to = key2pos(dest);
-	const vect = sub(to, from);
-	var n = norm(vect);
-	
-	if (n > 0) {
-		var uvect = div(n, vect);
-		const neighVectors = getNeighVectors();
+export const computeMoveVectorPostMove = (pieces: cg.Pieces, orig: cg.Key, dest: cg.Key): MoveVector | undefined => {
+	if (!pieces.has(orig) && pieces.has(dest)) {
+		const from = key2pos(orig);
+		const to = key2pos(dest);
+		const vect = sub(to, from);
+		var n = norm(vect);
 		
-		if (neighVectors.includes(uvect)) {// In-line move
-			//TODO
-		} else if (--n > 0) {
-			const rot = 360/neighVectors.length;
-			var found = false;
-			var vvect;
-			for (var _vect: Pos in neighVectors) {
-				const _nvect = mult(n, _vect);
-				vvect = getRotatedKeepNorm(_vect, rot);
+		if (n > 0) {
+			var uvect = div(n, vect);
+			const neighVectors = getNeighVectors();
+			
+			if (neighVectors.includes(uvect)) {// In-line move
+				var dests: cg.Key[] = [];
 				
-				if (add(_nvect, vvect) === vect) {
-					uvect = _vect;
-					found = true;
-					break;
-				} else {
-					vvect = getRotatedKeepNorm(_vect, -rot);
+				var a = to;
+				var ka = dest;
+				var k = 0;
+				
+				while (k < n) {
+					dests.push(ka);
+					
+					a = sub(to, mult(++k, uvect));
+					ka = pos2key(a);
+					
+					if (k < n - 1 && !pieces.has(ka)) return undefined;
+				}
+				
+				return {
+					directionString: getDirectionString(uvect),
+					landingSquares: dests
+				} as MoveVector;
+			} else if (--n > 0) {
+				const rot = 360/neighVectors.length;
+				var found = false;
+				var vvect;
+				for (var _vect: Pos in neighVectors) {
+					const _nvect = mult(n, _vect);
+					vvect = getRotatedKeepNorm(_vect, rot);
 					
 					if (add(_nvect, vvect) === vect) {
 						uvect = _vect;
 						found = true;
 						break;
-					}
-				}
-			}
-			
-			if (found) {// Broadside move
-				//TODO
-			}
-		}
-	}
-	
-	return undefined;
-	
-	
-	
-	
-	//TODO? Alex: the direction is just (dest - orig)/its norm (well, for in-line moves), but directionString is used in xhtml classes
-	const directionString = getDirectionString(dest, orig);
-	if (!directionString) return undefined;
-	const isAMoveInLine = isMoveInLine(dest, orig, directionString);
-	const inverseDirectionString = inverseDirection(directionString);
-	
-	if (isAMoveInLine) {
-		return {
-			directionString: inverseDirectionString,
-			landingSquares: [dest],//TODO Alex I think we should draw arrows on all moving marbles, hence all marbles from orig to dest (with the special case of the broadside moves)
-		};
-	}
-	
-	// side move
-	for (const lineDir of candidateLineDirs(directionString as DiagonalDirectionString)) {
-		const sideDirs = deducePotentialSideDirs(directionString as DiagonalDirectionString, lineDir);
-		const secondPos = move(dest, lineDir);
-		if (secondPos === undefined) continue;
-		for (const sideDir of sideDirs) {
-			const side2ndPos = move(secondPos, sideDir);
-			if (side2ndPos) {
-				const side1stPos = move(dest, sideDir);
-				if (side1stPos === undefined) continue;
-				if (side1stPos && pieces.get(secondPos)) {
-					if (side2ndPos === orig) {
-						return {
-							directionString: inverseDirection(sideDir),
-							landingSquares: [secondPos, dest],
-						};
 					} else {
-						// 3 marbles are moving
-						const thirdPos = move(secondPos, lineDir);
-						if (thirdPos === undefined) continue;
-						const side3rdPos = move(thirdPos, sideDir);
-						if (side3rdPos === undefined) continue;
-						if (pieces.get(thirdPos) && side3rdPos === orig) {
-							return {
-								directionString: inverseDirection(sideDir),
-								landingSquares: [secondPos, thirdPos, dest],
-							};
+						vvect = getRotatedKeepNorm(_vect, -rot);
+						
+						if (add(_nvect, vvect) === vect) {
+							uvect = _vect;
+							found = true;
+							break;
 						}
 					}
+				}
+				
+				if (found) {// Broadside move
+					var dests: cg.Key[] = [];
+					
+					var a = from;
+					var ka = orig;
+					var k = 0;
+					
+					while (k < n) {
+						const b = add(a, vvect);
+						const kb = pos2key(b);
+						if (!pieces.has(kb)) return undefined;
+						
+						dests.push(kb);
+						
+						a = add(from, mult(++k, uvect));
+						ka = pos2key(a);
+						if (k < n - 1 && pieces.has(ka)) return undefined;
+					}
+					
+					return {
+						directionString: getDirectionString(vvect),
+						landingSquares: dests
+					} as MoveVector;
 				}
 			}
 		}
