20e6d02ca6eea4e22dab416b896198006d2f48e9
Alex <alex.borello@direct-fleet.com>
2025-04-06 10:54:29 +0200

    [Abalone] whoops

--- src/variants/abalone/premove.ts
+++ src/variants/abalone/premove.ts
@@ -17,7 +17,7 @@ export const validDestinations = (variant: Variant, pieces: Pieces, orig: Key): 
 			for (const vect of neighVectors) {
 				let b = add(a, vect);
 				let dest = pos2key(b);
-				let bp: Piece = pieces.get(dest);
+				let bp = pieces.get(dest);
 				
 				let u = 1;
 				let max = false;
